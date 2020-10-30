ebc350b6f70478243f79765944542298a8ac8e4e
Q-gabe <gabriel.ong@u.nus.edu>
2020-10-31 03:13:23 +0800

    Fix build path resolution

--- server/deploy.sh
+++ server/deploy.sh
@@ -2,4 +2,4 @@
 
 yarn global add serverless
 yarn global add serverless-offline
-serverless deploy --stage prod --package $CODEBUILD_SRC_DIR/target/prod -v
+serverless deploy --stage prod --package $CODEBUILD_SRC_DIR/server/target/prod -v
